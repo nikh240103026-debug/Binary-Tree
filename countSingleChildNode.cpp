dbcff9a399346ba72e5bb84843c4546d82aa7359
Nikhil Raj <256415976+nikh240103026-debug@users.noreply.github.com>
2026-08-16 10:24:04 +0530

    Add files via upload

---
+++ countSingleChildNode.cpp
@@ -0,0 +1,68 @@
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+    int countSingleChildNode(Node* root){
+        if(root == nullptr){
+            return 0;
+        }
+
+        int left = countSingleChildNode(root->left);
+        int right = countSingleChildNode(root->right);
+
+        if((root->left != nullptr && root->right == nullptr) || (root->left == nullptr && root->right != nullptr)){
+            return left + right + 1;
+        }
+
+        return left + right;
+    }
+
+int main(){
+    Node* root = new Node(100);
+
+    root -> left = new Node(200);
+    root -> right = new Node(300);
+ 
+    root -> left -> left = new Node(400);
+    root -> left -> right = new Node(500);
+
+    root -> right -> left = new Node(600); 
+    root -> right -> right = new Node(700);
+
+    root ->left ->left ->left = new Node(800);
+
+    Node* root1= new Node(10);
+
+    root1 -> left = new Node(20);
+    root1 -> right = new Node(30);
+
+    root1 -> left -> left = new Node(40);
+    root1 -> left -> right = new Node(50);
+
+    Node* root2 = new Node(10);
+
+    root2 -> left = new Node(20);
+    root2 -> right = new Node(30);
+
+    root2 -> left -> left = new Node(40);
+    root2 -> left -> right = new Node(50);
+
+    cout<<"Number of single child nodes in given Tree: "<<countSingleChildNode(root);
+    cout<<endl;
+
+    return 0;
+}
